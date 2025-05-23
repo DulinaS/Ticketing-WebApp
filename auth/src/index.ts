5ac718dd42cf5ceffc44ee9ade49f1227931377e
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-05-24 03:14:39 +0530

    Introduce Express Validator

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
-import {json} from 'body-parser';
+import { json } from 'body-parser';
 
 //This is a named import, so we can import it in the index.ts file
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
 import { signUpRouter } from './routes/signup';
-
 
 const app = express();
 app.use(json());
@@ -17,7 +16,6 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
-
 app.listen(3000, () => {
-  console.log('Auth service listening on port 3000!!');
+  console.log('Auth service listening on port 3000!!!');
 });
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,13 +1,33 @@
-import express from "express";
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 //Router is a mini express application
 //What this does is create a new router object that can be used to define routes
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Signin");
-  
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req); //We use the validationResult function to get the errors from the request
+
+    //If there are errors, we send a 400 response with the errors
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body; //We get the email and password from the request body
+    console.log('Creating a user...');
+
+    res.send({});
+  }
+);
 
 export { router as signInRouter };
 //This is a named export, so we can import it in the index.ts file
