d5d78edf3ff83a529a0f1cc1eb6f82b1d8f3cc94
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-05-24 17:01:09 +0530

    Introduced serializedError() method

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,17 @@
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
+  //This is the status code that will be sent in the response
   reason = 'Error connecting to the database';
+  //This is the reason that will be sent in the response
+
   constructor() {
     super();
 
     //Only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,24 @@
 import { ValidationError } from 'express-validator';
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
+  //This is the status code that will be sent in the response
+
   constructor(public errors: ValidationError[]) {
     super();
 
     //Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((error) => {
+      if (error.type === 'field') {
+        return {
+          message: error.msg,
+          field: error.path,
+        };
+      }
+    });
   }
 }
