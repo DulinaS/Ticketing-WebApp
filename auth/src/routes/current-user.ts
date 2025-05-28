a9fc5453bbd7870179ca2383c56c1c3b346e374c
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-05-29 03:54:53 +0530

    feat: add requireAuth middleware to current user route for enhanced authentication

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,17 +1,22 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 //Router is a mini express application
 //What this does is create a new router object that can be used to define routes
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, (req, res) => {
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
 
   //Now the middleware does all
   //Checks if user logged out then sends a current user null
   //it extract payload and set it to user
+
+  //After that goes to requireAuth Middleware to check that user is successfully logged in
+  //and Act as currentUser.
+  //If not (req.currentUser == null) access isn't granted for the routes throws a error
 });
 
 export { router as currentUserRouter };
