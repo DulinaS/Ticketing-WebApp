e669e1bbab51e804f3ef82c2251647db1d8e7f30
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-06-19 01:22:19 +0530

    Handle input for signup form with event handlers

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,16 +1,36 @@
-export default() =>{
+import { useState } from 'react';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(email, password);
+  };
+
   return (
-    <form>
-        <h1>Sign UP</h1>
-        <div className="form-group">
-          <label>Email Address</label>
-          <input className="form-control" />
-        </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input type="password" className="form-control"/>
-        </div>
-        <buttom className = "btn btn-primary">Sign Up</buttom>
+    <form onSubmit={onSubmit}>
+      <h1>Sign UP</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
     </form>
   );
 };
