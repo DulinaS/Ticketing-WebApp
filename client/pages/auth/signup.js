162375daf70df538c43530d5a209453925a1e132
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-06-19 02:47:09 +0530

    Added error handling and fixed error showing UI

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,18 +4,23 @@ import axios from 'axios';
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]); //new state variable & function for errors
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
     //we pass email,password as second argument,
     //1st argument is the path
-    const response = await axios.post('/api/users/signup', {
-      email,
-      password,
-    });
-
-    console.log(response.data);
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (err) {
+      //Set the state of errors state variable using the error catched
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -38,6 +43,16 @@ export default () => {
           className="form-control"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops...</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.messsage}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
