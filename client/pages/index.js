a4180966fd713a2e9144a04b4382599e437d941d
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-06-20 02:52:40 +0530

    SSR example

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,12 @@
-export default () => {
-  return <h1>Landing Page</h1>;
+const LandingPage = ({ color }) => {
+  console.log('I am in the component', color); // Logs the color data received
+  return <h1>Landing Page</h1>; // Renders the landing page with the color data
 };
+
+LandingPage.getInitialProps = () => {
+  console.log('I am on the server'); // Logs when the server-side code is executed
+
+  return { color: 'red' }; // Returns the data (color) that will be passed to the component
+};
+
+export default LandingPage;
