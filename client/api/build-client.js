171860dff4d51cc1a394d22f28cd5204023b3153
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-06-21 04:17:39 +0530

    feat: refactor LandingPage to use buildClient for API calls and streamline server-side rendering

---
+++ client/api/build-client.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default ({ req }) => {
+  if (typeof window === 'undefined') {
+    //We are on server
+
+    return axios.create({
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    //We are on browser
+    // /---> base URL -> ticketing.dev
+    return axios.create({
+      baseURL: '/  ',
+    });
+  }
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
@@ -6,33 +6,10 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  //Window is obejct that exisits only on browser
-  if (typeof window === 'undefined') {
-    //If undefined we are on server
-    /* Request should be made to http://SERVICENAME.NAMESPACE/api/users/currentuser */
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
 
-    return data;
-  } else {
-    //In browser--> Navigating to another page
-    //If window defined, we are in browser
-    // Requests can be made with base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-
-    //Return Current userS
-    return data;
-  }
-  return {};
+  return data;
 };
 
 export default LandingPage;
-
-/* axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  }); */
