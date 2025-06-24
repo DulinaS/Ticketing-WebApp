d0c18224c2add12b31e8a4885307e386eabad40e
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-06-24 17:54:19 +0530

    Updates

--- common/build/index.js
+++ common/build/index.js
@@ -1,8 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const color = {
     red: 10,
     blue: 10,
     green: 10,
 };
 console.log(color);
-//Need to convert this ts file to js file
+exports.default = color;
