bb411eacf91776b577e9450e8cde67c566117966
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-12-04 21:35:30 +0530

    Added OrderCreatedListener to handle order creation events and enqueue expiration jobs. Introduced expiration queue to manage order expiration jobs. Created ExpirationCompletePublisher to publish expiration events. Defined ExpirationCompleteEvent interface for type safety.

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 //This is the function that will start the application and connect to the MongoDB database
 //We are using mongoose to connect to the MongoDB database
@@ -31,6 +32,8 @@ const start = async () => {
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
   } catch (err) {
     console.error(err);
   }
