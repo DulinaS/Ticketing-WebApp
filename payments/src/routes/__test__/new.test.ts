b860faea543cbc5d441189162264776db390e571
Dulina Senarathne <dulinahansa2002@gmail.com>
2025-12-09 01:09:13 +0530

    Add Payment model and implement payment record creation in payments service

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import { Order } from '../../models/order';
 import { OrderStatus } from '@dulinatickets/common';
 import { stripe } from '../../stripe';
+import { Payment } from '../../models/payment';
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
@@ -86,4 +87,43 @@ it('returns a 201 with valid inputs', async () => {
   });
 
   expect(stripeCharge).toBeDefined();
+});
+
+it('returns a 201 and creates a payment record', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  const price = Math.floor(Math.random() * 100000); //Random price for the order
+
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: userId,
+    version: 0,
+    price,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201);
+
+  //Get the list of charges created during the test
+  const charges = await stripe.charges.list({ limit: 50 });
+  const stripeCharge = charges.data.find((charge) => {
+    return charge.amount === price * 100;
+  });
+
+  expect(stripeCharge).toBeDefined();
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+
+  expect(payment).not.toBeNull();
 });
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,7 @@ import {
 } from '@dulinatickets/common';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payment';
 
 const router = express.Router();
 
@@ -41,11 +42,19 @@ router.post(
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
     //Here we would normally create a charge with Stripe
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'usd',
       amount: order.price * 100, //Stripe works with cents
       source: token,
     });
+
+    //If the charge is successful, we can create a payment record in our database
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+
+    await payment.save();
 
     res.status(201).send({ success: true });
   }
